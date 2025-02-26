5466a61bc62237c300a252601458618f58746dd3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-26 18:44:29 +0900

    mainを作って背景色を追加

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const TopPage = () => {
   return (
     <div className="relative">
-        <header className="leading-[50px] fixed top-0 left-0 right-0">
+        <header className="leading-[50px] fixed top-0 left-0 right-0 bg-white">
             <div className="container mx-auto flex justify-between">
                 <p className="logo">利用スケジュール</p>
                 <nav>
@@ -14,6 +14,17 @@ export const TopPage = () => {
                 </nav>
             </div>
         </header>
+        <main className="pt-[50px] bg-gradient-to-r from-lime-100 to-lime-200 h-screen">
+            <div>
+                <h1>スケジュール管理</h1>
+                <p>
+                    スケジュール管理をする
+                </p>
+                <div>
+                    <button>ログイン</button>
+                </div>
+            </div>
+        </main>
     </div>
   )
 }
