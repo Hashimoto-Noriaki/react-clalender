6dbdb97b4a0d636806fd0bc417a84822c735e48f
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-26 19:21:58 +0900

    トップ画面作成

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -14,14 +14,14 @@ export const TopPage = () => {
                 </nav>
             </div>
         </header>
-        <main className="pt-[50px] bg-gradient-to-r from-lime-100 to-lime-200 h-screen">
-            <div>
-                <h1>スケジュール管理</h1>
-                <p>
+        <main className="pt-[50px] bg-gradient-to-r from-lime-100 to-lime-200 h-screen flex flex-col justify-center items-center">
+            <div className="text-center">
+                <h1 className="logo text-7xl">スケジュール管理</h1>
+                <p className="text-5xl pt-[20vh]">
                     スケジュール管理をする
                 </p>
-                <div>
-                    <button>ログイン</button>
+                <div className="pt-[20vh]">
+                    <button className="bg-lime-800 text-white rounded-lg p-4 text-lg">ログイン</button>
                 </div>
             </div>
         </main>
