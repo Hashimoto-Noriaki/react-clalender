6cafe7ccfb9ba276d6873acec90054d0fefe8c83
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-26 18:01:13 +0900

    トップページの表示

---
+++ src/components/pages/TopPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const TopPage = () => {
+  return (
+    <div>
+        <header>
+            <p>利用スケジュール</p>
+            <nav>
+                <ul>
+                    <li>利用開始</li>
+                    <li>ログイン</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
