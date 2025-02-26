ba54c83668bc1fe81211be28e55aaae3b328bd65
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-26 17:39:18 +0900

    Reactの練習を真ん中に表示

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import '/styles/destyle.css'
+import './styles/index.css'
+import './styles/destyle.css'
+import './styles/output.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold underline">ハロー</h1>
+    </div>
   </React.StrictMode>,
 )
