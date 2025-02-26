a92dab5f14da2710912ad9d874a6d1a0b1b5aa21
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-26 18:20:59 +0900

    トップページの調整

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const TopPage = () => {
   return (
-    <div>
-        <header>
-            <div className="flex justify-between">
-                <p>利用スケジュール</p>
+    <div className="relative">
+        <header className="leading-[50px] fixed top-0 left-0 right-0">
+            <div className="container mx-auto flex justify-between">
+                <p className="logo">利用スケジュール</p>
                 <nav>
-                    <ul className="flex gap-5">
+                    <ul className="flex gap-5 text-lime-800">
                         <li>利用開始</li>
                         <li>ログイン</li>
                     </ul>
