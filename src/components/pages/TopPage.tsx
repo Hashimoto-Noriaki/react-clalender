e4589036ab6112f481d75ebb73369046ae007133
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-26 22:42:26 +0900

    NotLoginLayoutのリファクタリング

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,19 +1,8 @@
-import React from 'react'
+import { NotLoginLayout } from '../templates/NotLoginLayout';
 
 export const TopPage = () => {
   return (
-    <div className="relative">
-        <header className="leading-[50px] fixed top-0 left-0 right-0 bg-white">
-            <div className="container mx-auto flex justify-between">
-                <p className="logo">利用スケジュール</p>
-                <nav>
-                    <ul className="flex gap-5 text-lime-800">
-                        <li>利用開始</li>
-                        <li>ログイン</li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+    <NotLoginLayout>
         <main className="pt-[50px] bg-gradient-to-r from-lime-100 to-lime-200 h-screen flex flex-col justify-center items-center">
             <div className="text-center">
                 <h1 className="logo text-7xl">スケジュール管理</h1>
@@ -25,6 +14,6 @@ export const TopPage = () => {
                 </div>
             </div>
         </main>
-    </div>
+    </NotLoginLayout>
   )
 }
