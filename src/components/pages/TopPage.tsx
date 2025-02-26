90a00b324219bb15144293b5f05285ed382e1517
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-26 18:06:30 +0900

    トップページの表示(間隔を空ける)

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -4,13 +4,15 @@ export const TopPage = () => {
   return (
     <div>
         <header>
-            <p>利用スケジュール</p>
-            <nav>
-                <ul>
-                    <li>利用開始</li>
-                    <li>ログイン</li>
-                </ul>
-            </nav>
+            <div className="flex justify-between">
+                <p>利用スケジュール</p>
+                <nav>
+                    <ul className="flex gap-5">
+                        <li>利用開始</li>
+                        <li>ログイン</li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     </div>
   )
