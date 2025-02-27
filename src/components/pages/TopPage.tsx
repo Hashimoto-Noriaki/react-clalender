dfbc99795c9264f7da42c1e81cc15a4b08e25f1d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-27 15:47:55 +0900

    mainが不要だったので削除

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -2,14 +2,12 @@ import { PrimaryBtn } from '../atoms/PrimaryBtn.tsx';
 
 export const TopPage = () => {
   return (
-    <main className="pt-[50px] bg-gradient-to-r from-lime-100 to-lime-200 h-screen flex flex-col justify-center items-center">
-        <div className="text-center">
-            <h1 className="logo text-7xl">スケジュール管理</h1>
-            <p className="text-5xl pt-[20vh]">
-                スケジュール管理をする
-            </p>
-            <PrimaryBtn>ログイン</PrimaryBtn>
-        </div>
-    </main>
+    <div className="text-center">
+        <h1 className="logo text-7xl">スケジュール管理</h1>
+        <p className="text-5xl pt-[20vh]">
+            スケジュール管理をする
+        </p>
+        <PrimaryBtn>ログイン</PrimaryBtn>
+    </div>
   )
 }
