0866b1fd75a0b92360afe4ec725cc1586842a506
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-27 15:46:22 +0900

    Linkでログインとトップ画面に遷移

--- src/components/atoms/PrimaryBtn.tsx
+++ src/components/atoms/PrimaryBtn.tsx
@@ -6,8 +6,10 @@ type PropsType = {
 
 export const PrimaryBtn = ({ children }: PropsType) => {
   return (
-    <button className="bg-lime-800 text-white rounded-lg p-4 text-lg">
-        { children }
-    </button>
+    <div className="pt-[50px]">
+      <button className="bg-lime-800 text-white rounded-lg p-4 text-lg">
+          { children }
+      </button>
+    </div>
   )
 }
