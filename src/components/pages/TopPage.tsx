95626ebc207e3317f257d8a6869aad91438461d3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-27 16:24:58 +0900

    ログインボタンを押すとログイン画面に遷移される

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,13 +1,15 @@
+import { useNavigate } from 'react-router-dom'
 import { PrimaryBtn } from '../atoms/PrimaryBtn.tsx';
 
 export const TopPage = () => {
+  const navigate = useNavigate()
   return (
     <div className="text-center">
         <h1 className="logo text-7xl">スケジュール管理</h1>
         <p className="text-5xl pt-[20vh]">
             スケジュール管理をする
         </p>
-        <PrimaryBtn>ログイン</PrimaryBtn>
+        <PrimaryBtn onClick={() => navigate("/login")}>ログイン</PrimaryBtn>
     </div>
   )
 }
