01d2ceb093c1d1168c6f03734d217adc6b40cc05
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-28 17:24:25 +0900

    エラーメッセージ表示

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,24 +1,43 @@
 import { ChangeEvent,useState } from 'react'
 import { PrimaryBtn } from "../atoms/PrimaryBtn";
 import { Input } from '../atoms/Input';
+import { login } from '../../api/login'
+import { LoginInfoType } from '../../types/login'
 
 export const LoginPage = () => {
-    const [loginInfo,setLoginInfo] = useState<LoginInfoType>{
+    const [loginInfo,setLoginInfo] = useState<LoginInfoType>({
         email:"",
         password:"",
-    }
+    })
+
+    const [errorMessage,setErrorMessage] = useState("")
 
     const changeLoginInfo = (event: ChangeEvent<HTMLInputElement>)=> {
         const { name,value } = event.target;
-        setLoginInfo({...LoginInfo,[name]:value})
+        setLoginInfo({...loginInfo,[name]:value})
+    }
+
+    const handleLogin = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setErrorMessage("")
+        try {
+            login(loginInfo)
+        } catch {
+            setErrorMessage("ログインに失敗しました。")
+        }
     }
 
     return (
         <div className="w-[500px] bg-white rounded-lg shadow-lg py-10">
-            <form className="flex flex-col justify-center items-center gap-10">
+            <form className="flex flex-col justify-center items-center gap-10" onClick={handleLogin}>
             <h1 className="text-3xl text-lime-800 font-bold text-center">
                 ログイン
             </h1>
+            {errorMessage !== "" && (
+                <div className="p-5 text-white bg-red-500 w-[80%]">
+                    { errorMessage }
+                </div>
+            )}
             <div className="w-[80%]">
                 <Input
                     name="email"
@@ -37,7 +56,7 @@ export const LoginPage = () => {
                 onChange={changeLoginInfo}
                 />
             </div>
-            <PrimaryBtn>ログイン</PrimaryBtn>
+            <PrimaryBtn onClick={() => null}>ログイン</PrimaryBtn>
             </form>
         </div>
     );
