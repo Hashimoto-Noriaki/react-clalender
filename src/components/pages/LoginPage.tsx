d9867599a57946185c78ae48f459b3c4fe5189b3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-03 15:01:50 +0900

    ログイン機能の実装2

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,12 +1,14 @@
 import { ChangeEvent,FormEvent,useState } from 'react'
-import { PrimaryBtn } from "../atoms/PrimaryBtn";
 import { useSetRecoilState } from 'recoil'
+import {  useNavigate } from 'react-router-dom'
+import { PrimaryBtn } from "../atoms/PrimaryBtn";
 import { Input } from '../atoms/Input';
 import { login } from '../../api/login'
 import { LoginInfoType } from '../../types/login'
 import { loginUserState } from '../../store/loginUserState'
 
 export const LoginPage = () => {
+    const navigate = useNavigate()
     const setLoginUser = useSetRecoilState(loginUserState)
     const [loginInfo,setLoginInfo] = useState<LoginInfoType>({
         email:"",
@@ -28,6 +30,7 @@ export const LoginPage = () => {
         try {
             const resUser= login(loginInfo)
             setLoginUser({id:resUser.id, name:resUser.name})
+            navigate("/calendar")
         } catch {
             setErrorMessage("ログインに失敗しました。")
         }
