e2a26d94a4b67b7dd61d97f7bf79408751915867
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-03 15:46:16 +0900

    Recoil導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom'
+import  RecoilRoot from 'react'
 import './styles/index.css'
 import './styles/destyle.css'
 import './styles/output.css'
@@ -8,6 +9,8 @@ import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+    <RecoilRoot>
+      <RouterProvider router={router}/>
+    </RecoilRoot>
+  </React.StrictMode>
 )
