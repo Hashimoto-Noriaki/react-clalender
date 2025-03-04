e5a2f046121440ad56cacbffe75c2e238fae31d9
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-05 02:04:49 +0900

    ログイン後のレイアウト

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { TopPage } from './components/pages/TopPage';
 import { LoginPage } from './components/pages/LoginPage';
 import { NotLoginLayout } from './components/templates/NotLoginLayout';
 import { CalendarPage } from './components/pages/CalendarPage'
+import { LoginLayout } from './components/templates/LoginLayout'
 
 export const router = createBrowserRouter ([
     {path:"/",
@@ -10,7 +11,10 @@ export const router = createBrowserRouter ([
         children:[
             {index:true,element:<TopPage/>},
             {path:"/login",element:<LoginPage/>},
-            {path:"/calendar",element:<CalendarPage/>},
         ]
     },
+    {
+        element: <LoginLayout/>,
+        children: [{path:"/calendar",element:<CalendarPage/>}]
+    }
 ])
