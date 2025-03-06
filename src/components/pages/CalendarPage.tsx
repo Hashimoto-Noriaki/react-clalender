e53722f484257b982fd2eb2b913e28847bbb1953
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-06 12:08:43 +0900

    カレンダーの月、曜日のテーブルの作成

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,12 +1,22 @@
-import { useRecoilValue } from 'recoil'
-import { loginUserState } from '../../store/loginUserState'
+import { getMonth } from 'date-fns'
+import { DAYS_LIST } from '../../constants/calendar'
 
 export const CalendarPage = () => {
-    const loginUser = useRecoilValue(loginUserState)
+   const today = new Date()
   return (
-    <div>
-        <p>{loginUser.id}</p>
-        <p>{loginUser.name}</p>
-    </div>
+    <>
+        <h1 className="font-bold text-3xl mb-5">{`${(getMonth(today))+ 1}月`}</h1>
+        <table className="w-[80%] border-solid border-collapse border-2 border-lime-800 table-fixed">
+            <thead>
+                <tr className="bg-lime-800 text-white rounded-tl-lg rounded-tl-rg py-10">
+                    {DAYS_LIST.map((day) => (
+                        <th key={day} className="text-center text-xl py-3">
+                            {day}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+        </table>
+    </>
   )
 }
