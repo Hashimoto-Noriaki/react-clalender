66cf01b11ff75e9b2d8903764761a2fbbb29bd54
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-06 11:13:01 +0900

    月と曜日の表示のタグを作成

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,9 +1,23 @@
 import { getMonth } from 'date-fns'
+import { DAYS_LIST } from '../../constants/calendar'
 
 export const CalendarPage = () => {
     const today = new Date()
 
     return(
-        <h1 className="font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
+        <>
+            <h1 className="font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
+            <table>
+                <thead>
+                    <tr>
+                        {DAYS_LIST.map((day) =>(
+                            <th>
+                                {day}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+            </table>
+        </>
     )
 }
