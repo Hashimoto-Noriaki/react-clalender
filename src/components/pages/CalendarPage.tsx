544c82343ec0b0c8822d3d2b865f632b259d7d29
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-08 18:48:27 +0900

    CalendarHeaderの作成

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,6 +1,6 @@
 import { getMonth, eachWeekOfInterval, startOfMonth, endOfMonth, eachDayOfInterval, endOfWeek, getDate,isToday,isSameMonth } from 'date-fns';
-import { DAYS_LIST } from '../../constants/calendar';
 import { useEffect, useState } from 'react';
+import { CalendarHeader } from '../organisms/CalendarHeader'
 
 export const CalendarPage = () => {
     const today = new Date();
@@ -32,15 +32,7 @@ export const CalendarPage = () => {
         <>
             <h1 className="font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
             <table className="w-[80%] border-lime-800 border-solid border-2 border-collapse table-fixed">
-                <thead>
-                    <tr className="bg-lime-800 text-white text-xl rounded-tr-lg rounded-tl-lg py-10">
-                        {DAYS_LIST.map((day) => (
-                            <th key={day} className="text-center text-xl py-3">
-                                {day}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
+                <CalendarHeader/>
                 <tbody>
                     {dateList.map((oneWeek) => (
                         <tr key={`week-${getDate(oneWeek[0])}`} className="mx-10">
