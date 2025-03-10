9bf12b736b0d984b10b3c9bc9da53558e3d9912f
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-10 12:04:36 +0900

    currentDateというstateとして管理できるようにする

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,18 +1,19 @@
 import { getMonth} from "date-fns";
+import { useState } from "react";
 import { CalendarHeader } from "../organisms/CalendarHeader";
 import { CalendarBody } from "../organisms/CalendarBody";
 import { useCalendar } from "../../hooks/useCalendar";
 
 export const CalendarPage = () => {
-  const today = new Date()
-  const { dateList } = useCalendar({ currentDate: today })
+  const [currentDate,setCurrentDate] = useState(new Date())
+  const { dateList } = useCalendar({ currentDate: currentDate })
 
   return (
     <>
       <h1 className=" font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
       <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
         <CalendarHeader />
-        <CalendarBody currentDate={today} dateList={dateList} />
+        <CalendarBody currentDate={currentDate} dateList={dateList} />
       </table>
     </>
   )
