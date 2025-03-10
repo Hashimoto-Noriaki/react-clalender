f96a53ec61c3330ec90b71311870df508ecac0f3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-10 19:00:12 +0900

    カレンダーのナビゲーションメニューを作成

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -44,7 +44,7 @@ export const useCalendar = ({currentDate}: PropsType) => {
             ]
         })
         setDateList(newDateList)
-    },[]);
+    },[currentDate]);
 
     return {
         dateList
